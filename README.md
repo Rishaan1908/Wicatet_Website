6f77f1a47cc6f62892773e35f8009439ccfb036d
Gaurav Singh <singh975@purdue.edu>
2024-05-16 20:48:49 -0700

    map

---
+++ README.md
@@ -0,0 +1,15 @@
+HOME - 
+    Google maps location - Gaurav
+    Gallery slideshow - Riri
+    Basic Description  - Parht 
+ABOUT - 
+    Description of owners
+    Hisotry
+    Contact Info
+Contact Page  - anj
+
+FAQ
+    
+Shopping cart + products page + login
+    Comming soon
+Header/navbar - gaurav
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,32 +1,26 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
-import Particle from "../Particle";
-import Home2 from "./Home2";
-import Type from "./Type";
+import homeLogo from "../../Assets/logo.webp";
+import Map from "./Map"; 
 
 function Home() {
   return (
     <section>
       <Container fluid className="home-section" id="home">
-        <Particle />
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
+              <h1 style={{ paddingBottom: 15, fontFamily: 'cursive' }} className="heading">
+                Wilcatet Farms
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+              
+                <strong className="main-name"> Plant Nursery</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
+                {/* Additional content */}
               </div>
             </Col>
 
@@ -39,9 +33,16 @@ function Home() {
               />
             </Col>
           </Row>
+
+          <Row>
+            <Col md={12}>
+              <Map/> {/* Render the MapComponent here */}
+            </Col>
+          </Row>
         </Container>
       </Container>
-      <Home2 />
+
+
     </section>
   );
 }
