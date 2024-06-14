b44664c500e6aa954ac6e956da62acbf0c437ffb
Gaurav Singh <singh975@purdue.edu>
2024-06-14 01:02:56 -0700

    revamped slideshow

--- src/components/Home/Gallery.js
+++ src/components/Home/Gallery.js
@@ -1,49 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 
 function DisplayGallery() {
   const numImages = 6;
   const [images, setImages] = useState([]);
 
-  // Function to randomize images
-  const randomizeImages = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
-
-  // Initialize images array and set interval to randomize
+  // Initialize images array
   useEffect(() => {
-    // Initialize images array
     const initialImages = Array.from({ length: numImages }, (_, index) => index);
-    setImages(randomizeImages([...initialImages]));
-
-    const interval = setInterval(() => {
-      setImages(randomizeImages([...initialImages]));
-    }, 3000); // Randomize order every 3 seconds
-
-    return () => clearInterval(interval);
-  }, []);
-
+    setImages(initialImages);
+  }, []); 
   return (
-    <Container className="gallery-container">
-      <Row>
-        {images.map((index) => (
-          <Col key={index} md={4} className="mb-4">
-            <div style={{ width: "100%", height: "500px", overflow: "hidden" }}>
-            <img
-              src={require(`../Products/ProductImages/image${index + 1}.jpeg`)}
-              alt={`Number ${index + 1}`}
-              className="img-fluid"
-              style={{ width: "100%", height: "auto" }}
-            /> 
-            </div>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <Carousel interval={2000}>
+  {images.map((index) => (
+    <Carousel.Item key={index}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <img
+          src={require(`../Products/ProductImages/image${index + 1}.jpeg`)}
+          alt={`Number ${index + 1}`}
+          style={{ width: "50%", height: "auto" }}
+        />
+      </div>
+    </Carousel.Item>
+  ))}
+</Carousel>
   );
 }
 
