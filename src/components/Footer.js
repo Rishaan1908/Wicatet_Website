262ca429c9ede273bc6b0f417681fc243134475e
Gaurav Singh <singh975@purdue.edu>
2024-12-22 19:01:29 -0800

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,30 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copyright">
-          <h3>Designed and Developed by Parth Kapila, Rishaan Ponna, Gaurav Singh, Anjan Nanisetti</h3>
+          <h3>
+            Designed and Developed by{" "}
+            <a href="https://www.linkedin.com/in/parthkapila/" target="_blank" rel="noopener noreferrer">
+              Parth Kapila
+            </a>
+            ,{" "}
+            <a href="https://www.linkedin.com/in/rishaan-ponna/" target="_blank" rel="noopener noreferrer">
+              Rishaan Ponna
+            </a>
+            ,{" "}
+            <a href="https://www.linkedin.com/in/gaurav-gsingh/" target="_blank" rel="noopener noreferrer">
+              Gaurav Singh
+            </a>
+            , and{" "}
+            <a href="https://www.linkedin.com/in/anjan-nanisetti/" target="_blank" rel="noopener noreferrer">
+              Anjan Nanisetti
+            </a>
+          </h3>
         </Col>
         <Col md="4" className="footer-copyright">
           <h3>Copyright © {year}</h3>
         </Col>
         <Col md="4" className="footer-copyright">
-          <h3>C:+1 000-000-000</h3>
+          <h3>C:+1 352-973-2244</h3>
         </Col>
       </Row>
     </Container>
